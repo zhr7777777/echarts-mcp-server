0f830106345789a83cc3fe9421ca3886c490d216
hustcc <i@hust.cc>
2025-06-10 12:32:20 +0800

    build: export declaration (#83)

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ export function createServer(): Server {
   const server = new Server(
     {
       name: "mcp-server-chart",
-      version: "0.5.0",
+      version: "0.5.1",
     },
     {
       capabilities: {
